956831ae6e382110f1559f9bf1dfde4056504e5c
hudchant <hudchant@gmail.com>
2026-02-12 19:33:59 -0800

    Add unique id number to keep track of books

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Array to hold user's book collection
+const myLibrary = [];
+
 // Constructor for book objects
 function Book(title, author, pages, read) {
 
+    this.id = crypto.randomUUID();
     this.title = title; // Book title
     this.author = author; // Book author
     this.pages = pages; // Number of pages
     this.read = read; // Whether or not the book has been read yet
+
 
 
     this.info = function() { // Function that returns all book info in a single line
@@ -12,3 +17,7 @@ function Book(title, author, pages, read) {
     }
 }
 
+// Function for creating new books and storing them in myLibrary array
+function addBookToLibrary() {
+
+}
