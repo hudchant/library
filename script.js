b1f5d6086e6d155252bd3f97b32dbeeed00552c9
hudchant <hudchant@gmail.com>
2026-02-13 17:00:10 -0800

    Add function for creating and storing new books

--- script.js
+++ script.js
@@ -10,14 +10,20 @@ function Book(title, author, pages, read) {
     this.pages = pages; // Number of pages
     this.read = read; // Whether or not the book has been read yet
 
-
-
-    this.info = function() { // Function that returns all book info in a single line
+    this.info = function () { // Function that returns all book info in a single line
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read} `;
     }
 }
 
-// Function for creating new books and storing them in myLibrary array
-function addBookToLibrary() {
-
+// Function for creating new books and storing them
+function addBookToLibrary(bookTitle, bookAuthor, numOfPages, readingStatus) {
+    // Create new book
+    let newBook = new Book(bookTitle, bookAuthor, numOfPages, readingStatus);
+    // If new book object is created, push it into myLibrary array
+    if (newBook) {
+        myLibrary.push(newBook);
+    }
+    // return new book details
+    return newBook;
 }
+
