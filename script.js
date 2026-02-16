4abd4c84889ef82b47f0141311782bd3f71cbbf7
hudchant <hudchant@gmail.com>
2026-02-15 18:44:25 -0800

    Add styles for displaying books on UI

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ function Book(title, author, pages, read) {
     this.read = read; // Whether or not the book has been read yet
 
     this.info = function () { // Function that returns all book info in a single line
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read} `;
+        return `<div class='book-entry'>
+        <div class='book-title'>${this.title}</div> 
+        <div class='book-author'>by ${this.author}</div>
+        <div class='num-of-pages'>${this.pages} pages</div>
+        <div class='read-or-unread'>${this.read}</div>
+        </div>`;
     }
 }
 
@@ -28,12 +33,12 @@ function addBookToLibrary(bookTitle, bookAuthor, numOfPages, readingStatus) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-
     // For each loop that creates a new li element for each book pushed into myLibrary array
     myLibrary.forEach(book => {
         const ul = document.querySelector('ul'); // Ul reference
         const li = document.createElement('li'); // Create new li
-        li.textContent = book.info(); // Call .info() method from Book object to display books on page
+        li.classList.add('card'); // Create class name for li's to be referenced in css
+        li.innerHTML = book.info(); // Call .info() method from Book object to display books on page
         ul.appendChild(li); // Append books to ul element
     });
 });
@@ -42,6 +47,16 @@ document.addEventListener('DOMContentLoaded', function () {
 addBookToLibrary('Matilda', 'Roald Dahl', 255, 'Unread');
 addBookToLibrary('The Hunger Games', 'Suzanne Collins', 565, 'Read');
 addBookToLibrary('Divergent', 'Veronica Roth', 478, 'Read');
+addBookToLibrary('Chronicles of Narnia: The Lion, The Witch, and the Wardrobe', 'An Author', 478, 'Read');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('This is a really long book title, a super long book title', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+addBookToLibrary('Walk Two Moons', 'Some Author', 255, 'Unread');
+
 console.log(myLibrary);
 
 
