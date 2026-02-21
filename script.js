1a30c7eca964ee0241df61b5c2abe5d4a2d81e03
hudchant <hudchant@gmail.com>
2026-02-20 17:53:07 -0800

    Add delete button to remove book in a single click

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ function Book(title, author, pages, read) {
     this.pages = pages; // Number of pages
     this.read = read; // Whether or not the book has been read yet
 
-    this.info = function () { // Function that returns all book info in a single line
+    this.info = function () { // Function that returns all book details
         return `<div class='book-entry'>
         <div class='book-title'>${this.title}</div> 
         <div class='book-author'>by ${this.author}</div>
         <div class='num-of-pages'>${this.pages} pages</div>
         <div class='read-or-unread'>${this.read}</div>
+        <div class='remove-book'><button class='delete-btn'>X</div></div>
         </div>`;
     }
 }
@@ -29,26 +30,33 @@ function addBookToLibrary(bookTitle, bookAuthor, numOfPages, readingStatus) {
     if (newBook) {
         myLibrary.push(newBook);
     }
-    // Show all books in myLibrary collection
+    // Call function to show all books
     showBooks();
 }
 
 // Function for displaying books
 function showBooks() {
     const ul = document.querySelector('ul'); // Ul reference
-    ul.innerHTML = ''; // Clear ul after adding a book to make way for the next entry
-    myLibrary.forEach(book => {
+    ul.innerHTML = ''; // Clear ul after adding a book to prepare for the next entry
+    myLibrary.forEach((book, index) => {
         const li = document.createElement('li'); // Create new li
         li.classList.add('card'); // Create class name for li's to be referenced in css
         li.innerHTML = book.info(); // Call .info() method from Book object to display books on page
         ul.appendChild(li); // Append books to ul element
+
+        const deleteButton = li.querySelector('.delete-btn'); // Reference to delete button
+        deleteButton.addEventListener('click', () => {
+            // When a book's delete button is clicked, delete book at that particular index of myLibrary array
+            myLibrary.splice(index, 1);
+            showBooks(); // Refresh the book display after book has been deleted
+        });
     });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     const bookButton = document.querySelector('.book-btn'); // Reference to the 'Add New Book' button
     const modalDialog = document.querySelector('#modal-dialog'); // Reference to the modal dialog
-    const cancelButton = document.querySelector('#cancel-btn'); // Reference to form cancel button
+    const cancelButton = document.querySelector('#cancel-btn'); // Reference to form cancellation button
     const modalForm = modalDialog.querySelector('form'); // Reference to the form element
 
     // Event that opens the modal form when user clicks 'Add New Book' button
@@ -69,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Call function to add a new book
         addBookToLibrary(bookTitle, bookAuthor, bookPages, readOrNot);
 
-        // Close and reset modal dialog form
+        // Close and reset modal dialog form after a book submission
         modalDialog.close();
         modalForm.reset();
     });
@@ -79,8 +87,6 @@ document.addEventListener('DOMContentLoaded', function () {
         modalDialog.close();
     });
 });
-
-
 
 
 
